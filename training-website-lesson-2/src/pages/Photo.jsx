72180025b7366333c5c9e8d3d4797896166bfeef
Serhiy-Bevziuk <bevzjuk235@gmail.com>
2025-05-23 07:08:21 +0300

    Update Photo.jsx

--- training-website-lesson-2/src/pages/Photo.jsx
+++ training-website-lesson-2/src/pages/Photo.jsx
@@ -13,7 +13,7 @@ function Photo() {
 
   return (
     <main class="container py-4">
-      <div id="pandaCarousel" class="carousel slide" data-bs-ride="carousel">
+      <div id="tigerCarousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#pandaCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#pandaCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -40,11 +40,11 @@ function Photo() {
             </a>
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#pandaCarousel" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#tigerCarousel" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#pandaCarousel" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#tigerCarousel" data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
